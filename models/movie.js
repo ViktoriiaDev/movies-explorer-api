82058cc0a7f917ca49405beb3645e9e8333d9001
Viktoria Kotenko <kvi6605@gmail.com>
2022-10-23 23:18:16 +0300

    adedd models

---
+++ models/movie.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+
+const urlRegexp = /^((http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/;
+
+const movieSchema = new mongoose.Schema({
+  // страна создания фильма
+  country: {
+    type: String,
+    required: true,
+  },
+  // режиссёр фильма
+  director: {
+    type: String,
+    required: true,
+  },
+  // длительность фильма
+  duration: {
+    type: Number,
+    required: true,
+  },
+  // год выпуска фильма
+  year: {
+    type: String,
+    required: true,
+  },
+  // описание фильм
+  description: {
+    type: String,
+    required: true,
+  },
+  // ссылка на постер к фильму
+  image: {
+    type: String,
+    validate: {
+      validator(value) {
+        return urlRegexp.test(value);
+      },
+      message: 'Некорректный url',
+    },
+  },
+  // ссылка на трейлер фильма
+  trailerLink: {
+    type: String,
+    validate: {
+      validator(value) {
+        return urlRegexp.test(value);
+      },
+      message: 'Некорректный url',
+    },
+  },
+  // миниатюрное изображение постера к фильму
+  thumbnail: {
+    type: String,
+    validate: {
+      validator(value) {
+        return urlRegexp.test(value);
+      },
+      message: 'Некорректный url',
+    },
+  },
+  // _id пользователя, который сохранил фильм
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+  },
+  // id фильма, который содержится в ответе сервиса MoviesExplorer
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  // название фильма на русском языке
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  // название фильма на английском языке
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  // имя пользователя
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+});
+
+// поиск пользователя по его почте
+// если почта найдена, сверяем пароли
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+
+          return user; // теперь user доступен
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
